7a5778aa31f9e2d9202de77b4290e78df98176cc
Taehyun <ohyeaha@hanmail.net>
2025-07-01 15:31:58 +0900

    getMinus

--- C_TEAM_CAL/main.cpp
+++ C_TEAM_CAL/main.cpp
@@ -3,11 +3,20 @@
 class Cal {
 public:
     //이곳에 코드 작성
+    int getMinus(int a, int b) { return a - b; }
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
     EXPECT_EQ(1, 1);
+}
+
+TEST(t1, t4) {
+    Cal c;
+
+    int actual = c.getMinus(2, 1);
+
+    EXPECT_EQ(1, actual);
 }
 
 int main() {
