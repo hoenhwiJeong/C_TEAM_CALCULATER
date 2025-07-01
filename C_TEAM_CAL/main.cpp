fc8ce8922600bfa1113286f77ed0d10a08ad4f3f
Hyunseok <lukiaeve@gmail.com>
2025-07-01 15:39:46 +0900

    add getDivide

--- C_TEAM_CAL/main.cpp
+++ C_TEAM_CAL/main.cpp
@@ -3,19 +3,16 @@
 class Cal {
 public:
     //이곳에 코드 작성
-    int getSumSum(int a, int b, int c) { 
-        return a + b + c; 
+    int getDvide(int a, int b) { 
+        return a / b; 
     }
 };
 
-//테스트 케이스 작성
-TEST(t1, t2) {
-    EXPECT_EQ(1, 1);
-}
-TEST(t1, TC5) {
+
+TEST(CalTest, DivideTC) {
     Cal cal;
-    int ret = cal.getSumSum(1, 2, 3);
-    EXPECT_EQ(6, ret);
+    int ret = cal.getDvide(4, 2);
+    EXPECT_EQ(2, ret);
 }
 
 int main() {
