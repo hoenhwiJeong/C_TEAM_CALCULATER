e093f6281c3d47db9585e744f8df61d73aba929d
hh92.jeong <jhh1472@nate.com>
2025-07-01 15:29:54 +0900

    getSumSum

--- C_TEAM_CAL/main.cpp
+++ C_TEAM_CAL/main.cpp
@@ -3,11 +3,21 @@
 class Cal {
 public:
     //이곳에 코드 작성
+
+    double getSumSum(double a, double b, double c) {
+        return a + b + c;
+    }
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
     EXPECT_EQ(1, 1);
+}
+
+
+TEST(Calculator, TC6) {
+    Cal cal;
+    EXPECT_EQ(6, cal.getSumSum(1,2,3));
 }
 
 int main() {
