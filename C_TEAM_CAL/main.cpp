120fd691de5efea13c33c85c3ad7c938d888657b
jeseong.kim <asdf@gmail.com>
2025-07-01 15:21:17 +0900

    함수 3번. Zerop 구현 + Unit Test 완료

--- C_TEAM_CAL/main.cpp
+++ C_TEAM_CAL/main.cpp
@@ -3,12 +3,39 @@
 class Cal {
 public:
     //이곳에 코드 작성
+    int getZegop(int a) { return a * a; };
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
     EXPECT_EQ(1, 1);
 }
+
+
+TEST(TSgetZegop, TC1) {
+    int exp = 1;
+    int a = 1;
+    Cal c;
+    int ret = c.getZegop(a);
+    EXPECT_EQ(exp, ret);
+}
+
+TEST(TSgetZegop, TC2) {
+    int exp = 0;
+    int a = 0;
+    Cal c;
+    int ret = c.getZegop(a);
+    EXPECT_EQ(exp, ret);
+}
+
+TEST(TSgetZegop, TC3) {
+    int exp = 4;
+    int a = -2;
+    Cal c;
+    int ret = c.getZegop(a);
+    EXPECT_EQ(exp, ret);
+}
+
 
 int main() {
     ::testing::InitGoogleMock();
