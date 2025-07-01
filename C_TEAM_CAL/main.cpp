20e5774c578025625579e22ba29c6d90e3cc212e
Hyunseok <lukiaeve@gmail.com>
2025-07-01 15:27:56 +0900

    add getsumsum and tc5

--- C_TEAM_CAL/main.cpp
+++ C_TEAM_CAL/main.cpp
@@ -3,11 +3,19 @@
 class Cal {
 public:
     //이곳에 코드 작성
+    int getSumSum(int a, int b, int c) { 
+        return a + b + c; 
+    }
 };
 
 //테스트 케이스 작성
 TEST(t1, t2) {
     EXPECT_EQ(1, 1);
+}
+TEST(t1, TC5) {
+    Cal cal;
+    int ret = cal.getSumSum(1, 2, 3);
+    EXPECT_EQ(6, ret);
 }
 
 int main() {
